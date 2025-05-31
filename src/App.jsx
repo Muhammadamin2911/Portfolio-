d39d1c64d72519a1785730224ddc745f0b75f5a2
Muhammadamin2911 <muhammadamingulomov207@gmail.com>
2025-05-31 11:21:54 +0500

    add

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Page1 from './components/page1/Page1'
+
+const App = () => {
+  return (
+    <div className='side'>
+      <Page1/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/page1/Page1.jsx
@@ -0,0 +1,10 @@
+import "./Page1.css"
+const Page1 = () => {
+  return (
+    <div className='page'>
+      <h1>MUHAMMADAMIN</h1>
+    </div>
+  )
+}
+
+export default Page1
